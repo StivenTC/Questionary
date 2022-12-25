861bc90ed542b53f5fb6b7a08db25aec61a323fb
Stiven Tovar <stivent367@gmail.com>
2022-12-24 19:11:27 -0500

    Home

--- src/scenes/Home/Home.js
+++ src/scenes/Home/Home.js
@@ -1,13 +1,34 @@
 import React from 'react';
+import { BsSnow2 as Snow } from 'react-icons/bs';
+import { SlFire as Fire } from 'react-icons/sl';
+import { SlFire as Intimacy } from 'react-icons/sl';
+import { SlFire as Connection } from 'react-icons/sl';
+import { GiPerspectiveDiceSixFacesRandom as Dice } from 'react-icons/gi';
+
 function Home() {
   return (
     <nav className="home">
       <ul>
-        <li>Rompehielos</li>
-        <li>Conexión</li>
-        <li>Intimidad</li>
-        <li>Incendiarias</li>
-        <li>Personalizadas</li>
+        <li>
+          <h2>Rompehielos</h2>
+          <Snow />
+        </li>
+        <li>
+          <h2>Conexión</h2>
+          <Connection />
+        </li>
+        <li>
+          <h2>Intimidad</h2>
+          <Intimacy />
+        </li>
+        <li>
+          <h2>Incendiarias</h2>
+          <Fire />
+        </li>
+        <li>
+          <h2>Personalizadas</h2>
+          <Dice />
+        </li>
       </ul>
     </nav>
   );
