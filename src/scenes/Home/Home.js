feeb071d0514145dc840d589e86c38df664a268d
Stiven Tovar <stivent367@gmail.com>
2022-12-25 19:08:45 -0500

    feat home options

--- src/scenes/Home/Home.js
+++ src/scenes/Home/Home.js
@@ -1,34 +1,16 @@
 import React from 'react';
-import { BsSnow2 as Snow } from 'react-icons/bs';
-import { SlFire as Fire } from 'react-icons/sl';
-import { SlFire as Intimacy } from 'react-icons/sl';
-import { SlFire as Connection } from 'react-icons/sl';
-import { GiPerspectiveDiceSixFacesRandom as Dice } from 'react-icons/gi';
+import { HomeItems } from 'utils/items';
 
 function Home() {
   return (
     <nav className="home">
       <ul>
-        <li>
-          <h2>Rompehielos</h2>
-          <Snow />
-        </li>
-        <li>
-          <h2>Conexión</h2>
-          <Connection />
-        </li>
-        <li>
-          <h2>Intimidad</h2>
-          <Intimacy />
-        </li>
-        <li>
-          <h2>Incendiarias</h2>
-          <Fire />
-        </li>
-        <li>
-          <h2>Personalizadas</h2>
-          <Dice />
-        </li>
+        {HomeItems.map((item, index) =>
+          <li key={index}>
+            <h2>{item.name}</h2>
+            {item.icon}
+          </li>
+        )}
       </ul>
     </nav>
   );
---
+++ src/utils/items.js
@@ -0,0 +1,33 @@
+import { BsSnow2 as Snow } from 'react-icons/bs';
+import { SlFire as Fire } from 'react-icons/sl';
+import { TbShieldLock as Intimacy } from 'react-icons/tb';
+import { BsHeart as Connection } from 'react-icons/bs';
+import { GiPerspectiveDiceSixFacesRandom as Dice } from 'react-icons/gi';
+
+export const HomeItems = [
+  {
+    name: 'Rompehielos',
+    icon: <Snow />,
+    link: ''
+  },
+  {
+    name: 'Conexión',
+    icon: <Connection />,
+    link: ''
+  },
+  {
+    name: 'Intimidad',
+    icon: <Intimacy />,
+    link: ''
+  },
+  {
+    name: 'Incendiarias',
+    icon: <Fire />,
+    link: ''
+  },
+  {
+    name: 'Personalizadas',
+    icon: <Dice />,
+    link: ''
+  }
+]
